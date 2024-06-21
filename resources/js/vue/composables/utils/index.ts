dee7dec74524c4caeee27638b8e23c528ba3b720
Art Karasev <2291826+goldfinch@users.noreply.github.com>
2024-06-21 12:01:46 +1200

    WIP

--- resources/js/vue/composables/utils/index.ts
+++ resources/js/vue/composables/utils/index.ts
@@ -1,4 +1,5 @@
 export { default as useFormBuilder } from "../bundle-formkit/useFormBuilder";
 export { useAutocomplete } from "./src/autocomplete";
-export { googleMapsAPILoaded } from "./src/autocomplete";
+export { isGoogleMapsLoaded } from "./src/autocomplete";
+export { googleMapsInit } from "./src/autocomplete";
 export { default as useTest } from "./src/test";
--- resources/js/vue/composables/utils/src/autocomplete.ts
+++ resources/js/vue/composables/utils/src/autocomplete.ts
@@ -1,104 +1,61 @@
 import { usePlacesAutocomplete } from "@rootnode/vue-use-places-autocomplete";
-import { useElementVisibility, useScriptTag } from "@rootnode/@vueuse/core";
-import { ref, watch } from "vue";
+import { useScriptTag } from "@rootnode/@vueuse/core";
+import { ref } from "vue";
 
-// const acSet_jobAddress = ref(false)
-// const acSet_jobAddress_setValue = ref(false)
+const isGoogleMapsLoaded = ref(false);
 
-// const acSet_jobAddress_fn = (address, inputName) => {
-//   data.value[inputName] = address
-//   acSet_jobAddress.value = true
-//   acSet_jobAddress_setValue.value = address
-// }
-
-// watch(acSet_jobAddress_input, (n, o) => {
-//   if (acSet_jobAddress_setValue.value != n) {
-//     acSet_jobAddress.value = false
-//   }
-// })
-
-const googleMapsAPILoaded = ref(false);
-const suggestionsRef = ref(null);
-// const _input = ref(null)
-// const _options = ref(null)
-
-async function googleMapsInit() {
-  const apiKey = document.head.querySelector('meta[name="g-googleapis-key"]');
+const googleMapsInit = async () => {
+  const apiKey = document.head.querySelector('meta[name="g-cloud-key"]');
 
   if (apiKey) {
-    console.log("s1");
     await useScriptTag(
       "https://maps.googleapis.com/maps/api/js?key=" +
         apiKey.content +
         "&libraries=places",
-      // on script tag loaded.
       (el: HTMLScriptElement) => {
-        // do something
-        console.log("js loaded");
-        googleMapsAPILoaded.value = true;
+        isGoogleMapsLoaded.value = true;
       }
     );
-    console.log("s2");
-  }
-
-  return true;
-}
-
-const checkIfLoaded = async (current) => {
-  if (!googleMapsAPILoaded.value && current) {
-    await googleMapsInit();
   }
 
   return true;
 };
 
-const useAutocomplete = async (formData, inputName: string, options) => {
+const useAutocomplete = (emit, inputName: string, options) => {
   const input = ref("");
-  const inputRef = ref("");
+  const sugessionsPopup = ref(false);
 
-  const isVisible = useElementVisibility(inputRef);
-
-  await checkIfLoaded(isVisible);
-
-  watch(isVisible, (current) => {
-    checkIfLoaded(current);
-  });
-
-  console.log("last");
+  const setAddress = (v) => {
+    emit("updateValue", v);
+    sugessionsPopup.value = false;
+  };
 
   if (!options) {
-    const options = {
+    options = {
       debounce: 500,
       minLengthAutocomplete: 3,
+      apiOptions: {
+        language: "en",
+      },
+      autocompletionRequest: {
+        componentRestrictions: { country: "nz" },
+      },
     };
   }
 
-  // _input.value = input
-  // _options.value = options
-
-  watch(googleMapsAPILoaded, (current) => {
-    console.log("ggog1", current);
-
-    if (current && !suggestionsRef.value) {
-      // const { suggestions } = usePlacesAutocomplete(input, options);
-      // console.log("ggog2", suggestions.value);
-      // suggestionsRef.value = suggestions;
-    }
-    // checkIfLoaded(current);
-  });
+  const { suggestions } = usePlacesAutocomplete(input, options);
 
   const iname = inputName + "_input";
   const sname = inputName + "_suggestions";
-  const rname = inputName + "_ref";
-  const lname = inputName + "_scriptloaded";
+  const vname = inputName + "_visibility";
+  const setname = inputName + "_set";
 
   const r = {};
   r[iname] = input;
-  r[rname] = inputRef;
-  r[sname] = suggestionsRef;
-  r[lname] = googleMapsAPILoaded;
-  r["usePlacesAutocomplete"] = usePlacesAutocomplete;
+  r[sname] = suggestions;
+  r[vname] = sugessionsPopup;
+  r[setname] = setAddress;
   return r;
 };
 
-export { googleMapsAPILoaded, useAutocomplete };
+export { isGoogleMapsLoaded, useAutocomplete, googleMapsInit };
