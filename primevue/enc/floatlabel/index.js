e0da431b585d473559ce7ae893855a8c5db1672e
Art Karasev <2291826+goldfinch@users.noreply.github.com>
2024-07-23 13:20:14 +1200

    add clean preset

--- primevue/enc/floatlabel/index.js
+++
@@ -1,8 +0,0 @@
-export default {
-    root: {
-        color: '{form.field.float.label.color}',
-        focusColor: '{form.field.float.label.focus.color}',
-        invalidColor: '{form.field.float.label.invalid.color}',
-        transitionDuration: '0.2s'
-    }
-};
